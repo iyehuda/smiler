0b1985f443199b0786f7295e4aac5c0a11d8e956
Yehuda Chikvashvili <yehudaac1@gmail.com>
2019-12-05 00:52:28 +0200

    Add package metadata
    It is important to know information about the package.
    Added author details, project source and description.

--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="smiler",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
+    author="Yehuda Chikvashvili",
+    author_email="yehudaac1@gmail.com",
+    description="Emoji for humans",
+    keywords="smile emoji face",
+    url="https://github.com/iyehuda/smiler",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
+    ]
 )
